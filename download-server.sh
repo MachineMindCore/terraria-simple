0b7be00152eb3a2644db6a707d6aed6e849432b6
unknown <user.name>
2024-11-13 00:24:49 -0500

    init

---
+++ download-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir ~/games/terraria
+cd ~/games/terraria
+wget https://terraria.org/api/download/pc-dedicated-server/terraria-server-1441.zip
+unzip unzip terraria-server-1441.zip
+chmod +x Linux/TerrariaServer
